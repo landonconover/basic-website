c6fcbaa925264437ca2eb887b2b44631f483695d
Landon Conover <landon.conover@gmail.com>
2015-04-20 23:19:19 -0600

    added gulp wiredep

---
+++ Gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var wiredep = require('wiredep').stream;
+
+gulp.task('bower', function () {
+  gulp.src('app/index.html')
+    .pipe(wiredep())
+    .pipe(gulp.dest('app'));
+});
